794873b5b17aa9d287ac04a4d00831ccb09069fa
RathishMCA <rathishkumarmca@gmail.com>
2023-11-01 00:54:00 +0000

    The Full Program Was Changed..... Newly Created For Array Program

--- Sample/Java1st.java
+++ Sample/Java1st.java
@@ -4,13 +4,20 @@ class Java1st
 {
     public static void main(String[] arg)
     {
-        System.out.println("Enter The Value");
+        System.out.println("Array Length Of Value");
         Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-        int Add = num2 + 78;
-        System.out.println("Total is " + Add);
-        System.out.println("This Program is OVer");
-        System.out.println(" Adding The Another One Line");
+        int Length = scan.nextInt();
+
+        
+        int[] arr = new int[Length];
+        for(int i = 0 ; i<=Length;i++)
+        {
+            arr[i] = scan.nextInt();
+            System.out.println("Array " + i + " Values is" + arr[i]);
+        }
+
+        scan.close();
+
+
     }
 }
