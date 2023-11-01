4f6c4b783fff9b72ddecdb2561304d74d857c0b8
RathishMCA <rathishkumarmca@gmail.com>
2023-11-01 00:45:58 +0000

    Num2 Change to Static Number

--- Sample/Java1st.java
+++ Sample/Java1st.java
@@ -8,7 +8,7 @@ class Java1st
         Scanner scan = new Scanner(System.in);
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
-        int Add = num2 + num1;
+        int Add = num2 + 78;
         System.out.println("Total is " + Add);
         System.out.println("This Program is OVer");
         System.out.println(" Adding The Another One Line");
