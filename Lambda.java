b89520d4843e07e88e982aba5ac26c45cea724db
RathishMCA <rathishkumarmca@gmail.com>
2023-11-07 07:36:29 +0530

    Java Lambda Program

---
+++ Lambda.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Lambda {
+  public static void main(String[] args) {
+    ArrayList<Integer> numb = new ArrayList<Integer>();
+    numb.add(5);
+    numb.add(9);
+    numb.add(8);
+    numb.add(1);
+    numb.forEach( (n) -> {
+        
+     } );
+  }
+}
